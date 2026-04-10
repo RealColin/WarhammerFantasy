f032c93fd1c7c7eaec51b437efbdfc86ed520f9e
Werner, Colin <crwgond@gmail.com>
2026-04-09 20:57:24 -0500

    Added waist part of leggings to NPC armor layer

--- src/main/java/realcolin/whmod/client/entity/renderer/NPCRenderer.java
+++ src/main/java/realcolin/whmod/client/entity/renderer/NPCRenderer.java
@@ -3,6 +3,7 @@ package realcolin.whmod.client.entity.renderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
 import realcolin.whmod.WHMod;
 import realcolin.whmod.client.entity.model.NPCModel;
 import realcolin.whmod.entity.npc.NPC;
@@ -23,6 +24,7 @@ public class NPCRenderer<R extends LivingEntityRenderState & GeoRenderState> ext
             private final List<RenderData> BONES = List.of(
                     RenderData.head("armor_head"),
                     RenderData.body("armor_body"),
+                    new RenderData("armor_waist", EquipmentSlot.LEGS, model -> model.body),
                     RenderData.leftArm("armor_left_arm"),
                     RenderData.rightArm("armor_right_arm"),
                     RenderData.leftLeg("armor_left_leg"),
