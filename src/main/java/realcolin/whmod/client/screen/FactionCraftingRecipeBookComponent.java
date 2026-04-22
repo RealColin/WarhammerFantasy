dafca051855b71c31823d954cbf353dc6889cda0
Werner, Colin <crwgond@gmail.com>
2026-04-21 22:33:51 -0500

    better tabs for faction crafting table

--- src/main/java/realcolin/whmod/client/screen/FactionCraftingRecipeBookComponent.java
+++ src/main/java/realcolin/whmod/client/screen/FactionCraftingRecipeBookComponent.java
@@ -10,12 +10,13 @@ import net.minecraft.util.context.ContextMap;
 import net.minecraft.world.entity.player.StackedItemContents;
 import net.minecraft.world.inventory.AbstractCraftingMenu;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.RecipeBookCategories;
 import net.minecraft.world.item.crafting.display.RecipeDisplay;
 import org.jspecify.annotations.NonNull;
 import realcolin.whmod.faction.Faction;
+import realcolin.whmod.item.WHItems;
 import realcolin.whmod.item.recipe.FactionShapedCraftingRecipeDisplay;
 import realcolin.whmod.item.recipe.FactionShapelessCraftingRecipeDisplay;
 import realcolin.whmod.item.recipe.WHRecipeBookCategories;
@@ -26,18 +27,26 @@ import java.util.Optional;
 public class FactionCraftingRecipeBookComponent extends RecipeBookComponent<AbstractCraftingMenu> {
     private static final WidgetSprites FILTER_BUTTON_SPRITES = new WidgetSprites(Identifier.withDefaultNamespace("recipe_book/filter_enabled"), Identifier.withDefaultNamespace("recipe_book/filter_disabled"), Identifier.withDefaultNamespace("recipe_book/filter_enabled_highlighted"), Identifier.withDefaultNamespace("recipe_book/filter_disabled_highlighted"));
     private static final Component ONLY_CRAFTABLES_TOOLTIP = Component.translatable("gui.recipebook.toggleRecipes.craftable");
-    private static final List<RecipeBookComponent.TabInfo> TABS = List.of(
-            new RecipeBookComponent.TabInfo(new ItemStack(Items.COMPASS), Optional.empty(), WHSearchRecipeBookCategory.CRAFTING),
-            new RecipeBookComponent.TabInfo(Items.IRON_AXE, Items.GOLDEN_SWORD, WHRecipeBookCategories.FACTION_CRAFTING.get()),
-            new RecipeBookComponent.TabInfo(Items.BRICKS, RecipeBookCategories.CRAFTING_BUILDING_BLOCKS),
-            new RecipeBookComponent.TabInfo(Items.LAVA_BUCKET, Items.APPLE, RecipeBookCategories.CRAFTING_MISC),
-            new RecipeBookComponent.TabInfo(Items.REDSTONE, RecipeBookCategories.CRAFTING_REDSTONE));
 
     private final Faction faction;
 
     public FactionCraftingRecipeBookComponent(AbstractCraftingMenu menu, Faction faction) {
-        super(menu, TABS);
+        super(menu, makeTabs(faction));
         this.faction = faction;
+    }
+
+    private static List<RecipeBookComponent.TabInfo> makeTabs(Faction faction) {
+        Item item;
+        switch (faction) {
+            case EMPIRE -> item = WHItems.IMPERIAL_SWORD.get();
+            case DWARFS -> item = Items.IRON_INGOT;
+            default -> item = Items.GOLDEN_AXE;
+        }
+
+        return List.of(
+                new RecipeBookComponent.TabInfo(new ItemStack(Items.COMPASS), Optional.empty(), WHSearchRecipeBookCategory.CRAFTING),
+                new RecipeBookComponent.TabInfo(item, WHRecipeBookCategories.FACTION_CRAFTING.get())
+        );
     }
 
     @Override
